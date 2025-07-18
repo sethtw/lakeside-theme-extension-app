1f12d4d89af82afa7777d6d425d677352d4e7d2b
Seth Waltemyer <v-sewalt@microsoft.com>
2025-07-18 15:13:25 -0700

    start of cart transform extension

---
+++ extensions/demo-cart-transform-extension/src/cart_transform_run.ts
@@ -0,0 +1,12 @@
+import type {
+  CartTransformRunInput,
+  CartTransformRunResult,
+} from "../generated/api";
+
+const NO_CHANGES: CartTransformRunResult = {
+  operations: [],
+};
+
+export function cartTransformRun(input: CartTransformRunInput): CartTransformRunResult {
+  return NO_CHANGES;
+};
